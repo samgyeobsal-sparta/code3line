7a7706b742563dbf5b7c2d2eece45c3978f041a9
hyeeunKim <khu4237@gmail.com>
2024-06-18 20:40:47 +0900

    🐳 init commit

---
+++ src/main/java/sparta/code3line/Code3lineApplication.java
@@ -0,0 +1,13 @@
+package sparta.code3line;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Code3lineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Code3lineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sparta/code3line/Code3lineApplicationTests.java
@@ -0,0 +1,13 @@
+package sparta.code3line;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Code3lineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
