8e317de4be63620c4e9087080159baa8359b57e6
hyeeunKim <khu4237@gmail.com>
2024-06-20 00:05:49 +0900

    [#18] ✨ feat : 인가 로직 작성
    인가 관련 코드 추가

--- src/main/java/sparta/code3line/security/UserDetailsServiceImpl.java
+++ src/main/java/sparta/code3line/security/UserDetailsServiceImpl.java
@@ -4,13 +4,23 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import sparta.code3line.common.exception.CustomException;
+import sparta.code3line.common.exception.ErrorCode;
+import sparta.code3line.domain.user.entity.User;
+import sparta.code3line.domain.user.repository.UserRepository;
 
-@Component
+@Service
 @RequiredArgsConstructor
 public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new CustomException(ErrorCode.USERNAME_NOT_FOUND));
+
+        return new UserPrincipal(user);
     }
 }
