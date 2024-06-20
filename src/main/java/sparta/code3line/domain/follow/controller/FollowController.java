bfbf05806420cad9dbaf399eb0dbf4c487a6e1bc
LeeNaYoung240 <ajd0927@naver.com>
2024-06-20 17:43:24 +0900

    [#27] 🩹 fix : FollowController - 메서드 -> 파라미터
    getCurrentUser 메서드를 통해 현재 사용자를 직접 가져오는 대신 파라미터로 값을 받아오는 방식으로 수정

--- src/main/java/sparta/code3line/domain/follow/controller/FollowController.java
+++ src/main/java/sparta/code3line/domain/follow/controller/FollowController.java
@@ -4,12 +4,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import sparta.code3line.common.CommonResponse;
-import sparta.code3line.common.exception.CustomException;
-import sparta.code3line.common.exception.ErrorCode;
 import sparta.code3line.domain.follow.dto.FollowRequestDto;
 import sparta.code3line.domain.follow.dto.FollowResponseDto;
 import sparta.code3line.domain.follow.service.FollowService;
@@ -25,28 +22,28 @@ public class FollowController {
     private final FollowService followService;
 
     @PostMapping("/{followId}")
-    public ResponseEntity<CommonResponse<FollowResponseDto>> createFollow(@PathVariable Long followId, @RequestBody FollowRequestDto followRequestDto) {
-        followService.followUser(followRequestDto.getFollowingUserId(), getCurrentUser());
-        FollowResponseDto followResponseDto = new FollowResponseDto(followRequestDto.getFollowingUserId(), getCurrentUser().getId());
+    public ResponseEntity<CommonResponse<FollowResponseDto>> createFollow(
+            @PathVariable Long followId,
+            @RequestBody FollowRequestDto followRequestDto,
+            @AuthenticationPrincipal UserPrincipal userPrincipal) {
+
+        User currentUser = userPrincipal.getUser();
+        followService.followUser(followRequestDto.getFollowingUserId(), currentUser);
+        FollowResponseDto followResponseDto = new FollowResponseDto(followRequestDto.getFollowingUserId(), currentUser.getId());
         CommonResponse<FollowResponseDto> response = new CommonResponse<>("팔로우 성공 🎉", HttpStatus.OK.value(), followResponseDto);
         return ResponseEntity.ok().body(response);
     }
 
     @DeleteMapping("/{followId}")
-    public ResponseEntity<CommonResponse<FollowResponseDto>> deleteFollow(@PathVariable Long followId, @RequestBody FollowRequestDto followRequestDto) {
-        followService.unfollowUser(followRequestDto.getFollowingUserId(), getCurrentUser());
-        FollowResponseDto followResponseDto = new FollowResponseDto(followRequestDto.getFollowingUserId(), getCurrentUser().getId());
+    public ResponseEntity<CommonResponse<FollowResponseDto>> deleteFollow(
+            @PathVariable Long followId,
+            @RequestBody FollowRequestDto followRequestDto,
+            @AuthenticationPrincipal UserPrincipal userPrincipal) {
+
+        User currentUser = userPrincipal.getUser();
+        followService.unfollowUser(followRequestDto.getFollowingUserId(), currentUser);
+        FollowResponseDto followResponseDto = new FollowResponseDto(followRequestDto.getFollowingUserId(), currentUser.getId());
         CommonResponse<FollowResponseDto> response = new CommonResponse<>("언팔로우 성공 🎉", HttpStatus.OK.value(), followResponseDto);
         return ResponseEntity.ok().body(response);
-    }
-
-    public User getCurrentUser() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication == null || !authentication.isAuthenticated()) {
-            throw new CustomException(ErrorCode.USERNAME_NOT_FOUND.getStatus(), "인증되지 않은 사용자입니다.");
-        }
-
-        UserPrincipal userPrincipal = (UserPrincipal) authentication.getPrincipal();
-        return userPrincipal.getUser();
     }
 }
