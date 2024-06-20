4d0927c69497f069ca61a113a596fc7185360116
2minus <blecker0610@gmail.com>
2024-06-21 00:59:47 +0900

    [#11] 🩹 fix : 댓글 CRUD 짜잘 수정 (작동 확인 완료)

--- src/main/java/sparta/code3line/Code3lineApplication.java
+++ src/main/java/sparta/code3line/Code3lineApplication.java
@@ -2,8 +2,10 @@ package sparta.code3line;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class Code3lineApplication {
 
     public static void main(String[] args) {
