ecbc39a1fb4c2ba611692b71525941f04bc2ab27
2minus <blecker0610@gmail.com>
2024-06-20 16:14:24 +0900

    [#10] 🩹 fix : login 기능 수정 (작동 확인)

--- src/main/java/sparta/code3line/domain/user/controller/AuthController.java
+++ src/main/java/sparta/code3line/domain/user/controller/AuthController.java
@@ -1,9 +1,8 @@
 package sparta.code3line.domain.user.controller;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import sparta.code3line.common.CommonResponse;
@@ -22,17 +21,22 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/login")
-    public CommonResponse<LoginResponseDto> login(LoginRequestDto requestDto) throws IOException {
+    public ResponseEntity<CommonResponse<LoginResponseDto>> login(@RequestBody LoginRequestDto requestDto) throws IOException {
 
         LoginResponseDto responseDto = authService.login(requestDto);
+
         HttpHeaders headers = new HttpHeaders();
         headers.add("Authorization", responseDto.getAccessToken());
-        return new CommonResponse<LoginResponseDto>("로그인 성공", 200, responseDto);
+
+        CommonResponse<LoginResponseDto> response = new CommonResponse<LoginResponseDto>("로그인 성공", 200, responseDto);
+
+        return ResponseEntity.ok().headers(headers).body(response);
     }
 
     @PatchMapping("/logout")
-    public CommonResponse<Void> logout(@AuthenticationPrincipal UserPrincipal principal) {
-        return new CommonResponse<Void>("로그아웃 성공", 204, authService.logout(principal));
+    public ResponseEntity<CommonResponse<Void>> logout(@AuthenticationPrincipal UserPrincipal principal) {
+        CommonResponse<Void> response = new CommonResponse<Void>("로그아웃 성공", 204, authService.logout(principal));
+        return ResponseEntity.ok().body(response);
     }
 
 }
