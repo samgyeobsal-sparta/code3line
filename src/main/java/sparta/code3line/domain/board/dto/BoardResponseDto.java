3bc6d37aa530805bec6df065efcc1c78fa210afe
Imnotcoderdude <topsolo7@naver.com>
2024-06-20 15:14:26 +0900

    [#22]✨ feat : 게시물 생성 추가. 테스트 X

--- src/main/java/sparta/code3line/domain/board/dto/BoardResponseDto.java
+++ src/main/java/sparta/code3line/domain/board/dto/BoardResponseDto.java
@@ -1,4 +1,26 @@
 package sparta.code3line.domain.board.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
 public class BoardResponseDto {
+    private Long boardId;
+    private String title;
+    private String content;
+    private LocalDateTime createdAt;
+
+    public BoardResponseDto(
+            Long boardId,
+            String title,
+            String content,
+            LocalDateTime createdAt) {
+        this.boardId = boardId;
+        this.title = title;
+        this.content = content;
+        this.createdAt = createdAt;
+    }
 }
