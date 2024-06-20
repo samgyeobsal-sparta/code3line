8f57b6a0cd2321dec6226ec9c78b9c94b180414f
LeeNaYoung240 <ajd0927@naver.com>
2024-06-21 02:23:58 +0900

    [#27] ✨ feat : 팔로우, 언팔로우 기능 구현
    - 예외처리 추가, Timestamped 추가

--- src/main/java/sparta/code3line/domain/follow/service/FollowService.java
+++ src/main/java/sparta/code3line/domain/follow/service/FollowService.java
@@ -3,16 +3,13 @@ package sparta.code3line.domain.follow.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import sparta.code3line.common.exception.CustomException;
-import sparta.code3line.domain.follow.dto.FollowResponseDto;
 import sparta.code3line.domain.follow.entity.Follow;
 import sparta.code3line.domain.follow.repository.FollowRepository;
 import sparta.code3line.domain.user.entity.User;
 import sparta.code3line.domain.user.repository.UserRepository;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 import static sparta.code3line.common.exception.ErrorCode.*;
+
 
 @Service
 @RequiredArgsConstructor
@@ -23,15 +20,16 @@ public class FollowService {
 
     // 팔로우 기능
     public void followUser(Long followingUserId, User follower) {
-        if(followingUserId == null)
-        {
-            throw new CustomException(USERNAME_NOT_FOUND);
+
+        if (followingUserId == null) {
+            throw new CustomException(BOARD_NOT_FOUND);
         }
         if (followingUserId.equals(follower.getId())) {
-            throw new CustomException(USERNAME_NOT_FOUND);
+            throw new CustomException(COMMENT_NOT_FOUND);
         }
 
         User followingUser = findUser(followingUserId);
+        //User followingUser = findUser(follower.getId());
 
         if (isAlreadyFollowing(followingUserId, follower.getId())) {
             throw new CustomException(ALREADY_FOLLOW);
@@ -43,10 +41,12 @@ public class FollowService {
 
     // 언팔로우 기능
     public void unfollowUser(Long followingUserId, User follower) {
-        if(followingUserId == null)
-        {
+
+
+        if (followingUserId == null) {
             throw new CustomException(NOT_FOLLOWED);
         }
+
         Follow follow = followRepository.findByFollowingIdAndFollowerId(followingUserId, follower.getId())
                 .orElseThrow(() -> new CustomException(NOT_FOLLOWED));
 
@@ -54,17 +54,18 @@ public class FollowService {
     }
 
     // 팔로잉 목록 조회 기능
-    public List<FollowResponseDto> getFollowings(Long userId) {
+   /* public List<FollowResponseDto> getFollowings(Long userId) {
         User user = findUser(userId);
-        List<Follow> followingList = user.getFollowingList();
+        List<Follow> followingList = user.getFollowing();
 
         return followingList.stream()
                 .map(FollowResponseDto::new)
                 .collect(Collectors.toList());
-    }
+    }*/
 
     // 사용자 조회 메서드
     private User findUser(Long userId) {
+
         return userRepository.findById(userId)
                 .orElseThrow(() -> new CustomException(USERNAME_NOT_FOUND));
     }
